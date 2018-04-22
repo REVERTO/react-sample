c4bb3d9bac8f340f3f47d0cbc4857c5d40446d79
Tomoyuki Ito <ito@nagisa-inc.jp>
2018-04-22 18:53:54 +0900

    feat: add material-ui

--- src/App.js
+++ src/App.js
@@ -1,31 +1,63 @@
-// react
-import React, { Component } from 'react';
+// dependencies
+import React, { Component } from 'react'
 import { HashRouter as Router, Route, Link } from 'react-router-dom'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import { AppBar, MenuItem, Drawer } from 'material-ui'
 
-// components
-// import Home from './components/Home'
-// import About from './components/About'
-// import Friends from './components/Friends'
 import Landing from './components/Landing'
 import SignIn from './components/SignIn'
 
 class App extends Component {
+
+  constructor() {
+    super()
+    this.state = {
+      open: false
+    }
+  }
+
+  handleToggle() {
+    this.setState({
+      open: !this.state.open
+    })
+  }
+
   render() {
     return (
       <Router>
-        <div>
-          <ul>
-            <li><Link to='/'>Landing</Link></li>
-            <li><Link to='/signin'>SignIn</Link></li>
-          </ul>
-          <hr />
+        <MuiThemeProvider>
+          <div>
+            <Drawer
+              docked={false}
+              width={200}
+              open={this.state.open}
+              onRequestChange={ () => this.handleToggle() }
+            >
+              <MenuItem>Landing</MenuItem>
+              <MenuItem>SignIn</MenuItem>
+            </Drawer>
+            <AppBar
+              title="sample"
+              onLeftIconButtonClick={ () => this.handleToggle() }
+            />
+            <ul>
+              <li>
+                <Link to='/'>Landing</Link>
+              </li>
+              <li>
+                <Link to='/signin'>SignIn</Link>
+              </li>
+            </ul>
+            <hr/>
 
-          <Route exact path='/' component={Landing} />
-          <Route path='/signin' component={SignIn} />
-        </div>
+            <Route path='/' component={Landing}/>
+            <Route path='/signin' component={SignIn}/>
+          </div>
+        </MuiThemeProvider>
       </Router>
-    );
+    )
   }
+
 }
 
-export default App;
+export default App

ae77d68a2c2cb2dc92af38a340485ce3ade9d30d
Tomoyuki Ito <ito@nagisa-inc.jp>
2018-05-16 21:39:17 +0900

    style: remove commentouts

--- src/components/SignIn/index.js
+++ src/components/SignIn/index.js
@@ -1,20 +1,8 @@
 import React, { Component } from 'react'
 import { withRouter } from 'react-router'
 import { TextField, RaisedButton } from 'material-ui'
-// import * as firebase from 'firebase'
-// import 'firebase/auth'
 
 import firebase from '../../firebase'
-
-// var config = {
-//     apiKey: "AIzaSyCtL8D5vn5k0q43MnDbuynbDb1dWXZ5nFU",
-//     authDomain: "auth-sample-5db27.firebaseapp.com",
-//     databaseURL: "https://auth-sample-5db27.firebaseio.com",
-//     projectId: "auth-sample-5db27",
-//     storageBucket: "auth-sample-5db27.appspot.com",
-//     messagingSenderId: "315231003307"
-// };
-// firebase.initializeApp(config)
 
 class SignIn extends Component {
 
